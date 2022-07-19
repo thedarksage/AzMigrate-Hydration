566a505ce11a9c58c6e85b1b668a0a118190f84e
Shantanu Mishra <shamish@microsoft.com>
2022-07-19 16:50:44 +0530

    Checkin Build

--- .pipelines/BuildAzureRecoveryUtil.sh
+++ .pipelines/BuildAzureRecoveryUtil.sh
@@ -11,6 +11,12 @@ PatchVersion=`grep PatchVersion $ROOT/BuildConfig.bcf | cut -f 2 -d "="`
 BuildQuality=`grep BuildQuality $ROOT/BuildConfig.bcf | cut -f 2 -d "="`
 BuildPhase=`grep BuildPhase $ROOT/BuildConfig.bcf | cut -f 2 -d "="`
 
+pwd
+
+ls -l
+
+echo "$MajorVersion $MinorVersion $PatchsetVersion $PatchVersion $BuildQuality"
+
 gmake AzureRecoveryUtil X_VERSION_MAJOR=$MajorVersion X_VERSION_MINOR=$MinorVersion X_PATCH_SET_VERSION=$PatchsetVersion X_PATCH_VERSION=$PatchVersion X_VERSION_QUALITY=$BuildQuality X_VERSION_PHASE=$BuildPhase debug=no warnlevel=none verbose=yes partner=inmage > >(tee AzureRecoveryUtilBuild.log) 2>&1
 
 if [ $? -eq 0 ]; then
